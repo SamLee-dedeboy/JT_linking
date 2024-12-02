052cb2dbbba538b9b7ad60c9e45110c0154da0a3
SamLee-dedeboy <samytleezzz@gmail.com>
2024-12-02 14:41:12 -0800

    add template for selecting workbook scenario

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,13 @@
   import { onMount } from "svelte";
   import { server_address, categories } from "./constants";
   import CategoryOverview from "lib/CategoryOverview.svelte";
+  import WorkbookOverview from "lib/WorkbookOverview.svelte";
 </script>
 
 <main class="w-[100vw] h-[100vh] max-h-[100vh] p-2 flex">
-  <div class="flex-1">
+  <div class="flex-1 flex flex-col">
     <CategoryOverview></CategoryOverview>
+    <WorkbookOverview></WorkbookOverview>
   </div>
   <div class="flex-1"></div>
 </main>
---
+++ src/lib/WorkbookOverview.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  let selectedScenario = undefined;
+  let selectedCategory = undefined;
+  //   fetch()..
+</script>
+
+<div class="flex flex-col gap-y-2">
+  <div class="flex gap-x-4">
+    <select class="" onchange={(e) => (selectedScenario = e.target.value)}>
+      <option value="scenario 1">Scenario 1</option>
+      <option value="scenario 2">Scenario 2</option>
+      <option value="scenario 3">Scenario 3</option>
+    </select>
+    <select class="" onchange={(e) => (selectedCategory = e.target.value)}>
+      <option value="category 1">Category 1</option>
+      <option value="category 2">Category 2</option>
+      <option value="category 3">Category 3</option>
+    </select>
+  </div>
+  <div>
+    {selectedScenario || "Not Selected"} - {selectedCategory || "Not Selected"}
+  </div>
+</div>
